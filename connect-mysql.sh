7fe56448d9f55640d02564b3d9d8923581b10555
JavaCommons Technologies <javacommons@gmail.com>
2024-12-10 16:36:22 +0900

    .

--- connect-mysql.sh
+++ connect-mysql.sh
@@ -1,5 +1,4 @@
 #! /usr/bin/env bash
 set -uvx
 set -e
-mysql -h database-1.c5uq0yao8tep.ap-southeast-2.rds.amazonaws.com -P 3306 -u admin -p
-
+mysql -h database-2.chg4oqauqbrq.ap-northeast-1.rds.amazonaws.com -P 3306 -u admin -p
